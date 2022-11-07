06cbbc471a81057f30ad20245e3c616058b27739
dorukwagle <dorukwagle@gmail.com>
2022-11-07 07:20:45 +0545

    changes done to registration page

--- test.py
+++ test.py
@@ -10,3 +10,20 @@ label = Label(root, image=imag)
 label.pack()
 
 root.mainloop()
+
+# tab control
+
+# style = ttk.Style()
+# # style.map("TNotebook.Tab", background=[("", "white")])
+# style.configure('TNotebook.Tab', background="grey", foreground="black", padding=[25, 8], font=("", 13, "bold"))
+# style.configure('TNotebook', padding=[10, 10], background="grey")
+# create a Notebook to hold both registration form
+# self.tab_control = ttk.Notebook(base_frame, takefocus=0)
+# self.tab_control.bind("<<NotebookTabChanged>>", self.on_tab_change)
+# create tabs
+# customer_tab = ttk.Frame(self.tab_control)
+# driver_tab = ttk.Frame(self.tab_control)
+#
+# self.tab_control.add(customer_tab, text="Customer SignUp")
+# self.tab_control.add(driver_tab, text="Driver SignUp")
+# self.tab_control.pack(expand=1, fill="both")
--- views/register.py
+++ views/register.py
@@ -35,6 +35,11 @@ class RegistrationPage(ttk.Frame):
         self.__canvas.create_window(parent.get_width_pct(70), parent.get_height_pct(10), anchor=tk.NW,
                                     window=base_frame)
 
+        # create back button
+        back = cw.Button(self, text="< Back", takefocus=0, font=("", 15),
+                  fg="white", fg_pressed="grey", bg="#299617", bg_hover="#0a6522", bg_pressed="#043927")
+        self.__canvas.create_window(2, 2, window=back, anchor=tk.NW)
+
         # create intro label
         self.__text1 = self.__canvas.create_text(parent.get_width_pct(10), parent.get_height_pct(45),
                                                  text="", fill="white",
@@ -43,25 +48,8 @@ class RegistrationPage(ttk.Frame):
         self.__text2 = self.__canvas.create_text(parent.get_width_pct(20), parent.get_height_pct(51),
                                                  text="", fill="white", font=("", 30, "bold"),
                                                  anchor=tk.NW)
-
-        style = ttk.Style()
-        # style.map("TNotebook.Tab", background=[("", "white")])
-        style.configure('TNotebook.Tab', background="grey", foreground="black", padding=[25, 8], font=("", 13, "bold"))
-        style.configure('TNotebook', padding=[10, 10], background="grey")
-        # create a Notebook to hold both registration form
-        self.tab_control = ttk.Notebook(base_frame, takefocus=0)
-        self.tab_control.bind("<<NotebookTabChanged>>", self.on_tab_change)
-        # create tabs
-        customer_tab = ttk.Frame(self.tab_control)
-        driver_tab = ttk.Frame(self.tab_control)
-
-        self.tab_control.add(customer_tab, text="Customer SignUp")
-        self.tab_control.add(driver_tab, text="Driver SignUp")
-        self.tab_control.pack(expand=1, fill="both")
-
-        # add respective pages to the tabs
-        CustomerRegistration(customer_tab).pack()
-        DriverRegistration(driver_tab).pack()
+        # add registration page to the base frame
+        CustomerRegistration(base_frame).pack()
 
         self.pack()
         # start the event listener
@@ -152,61 +140,61 @@ class CustomerRegistration(ttk.Frame):
                   fg="white", fg_pressed="grey", bg="#299617", bg_hover="#0a6522", bg_pressed="#043927").pack()
 
 
-class DriverRegistration(ttk.Frame):
-    def __init__(self, container):
-        self.font = ("", 20)
-        style = ttk.Style()
-        style.configure("TFrame", background="#d4d4d4")
-        # create right frame to hold form box
-        super().__init__(container, style="TFrame", padding=25)
-
-        # create form elements
-        self.username = cw.InputBox(self, placeholder="Full Name", placeholder_color="#c3c3c3", font=self.font)
-        self.username.pack()
-        # add space
-        ttk.Label(self, text="", font=("", 2)).pack()
-
-        self.gender = ttk.Combobox(self, values=["Male", "Female"], background="white", font=("", 15), state="readonly")
-        self.gender.set("<<Select Gender>>")
-        self.gender.bind("<<ComboboxSelected>>", lambda event: combo_select(self.gender))
-        self.gender.pack(expand=1, fill="both")
-        # add space
-        ttk.Label(self, text="", font=("", 2)).pack()
-
-        self.username = cw.InputBox(self, placeholder="Email Address", placeholder_color="#c3c3c3", font=self.font)
-        self.username.pack()
-        # add space
-        ttk.Label(self, text="", font=("", 2)).pack()
-
-        self.username = cw.InputBox(self, placeholder="Address", placeholder_color="#c3c3c3", font=self.font)
-        self.username.pack()
-        # add space
-        ttk.Label(self, text="", font=("", 2)).pack()
-
-        self.username = cw.InputBox(self, placeholder="License ID", placeholder_color="#c3c3c3", font=self.font)
-        self.username.pack()
-        # add space
-        ttk.Label(self, text="", font=("", 2)).pack()
-
-        self.username = cw.InputBox(self, placeholder="Username", placeholder_color="#c3c3c3", font=self.font)
-        self.username.pack()
-        # add space
-        ttk.Label(self, text="", font=("", 2)).pack()
-        self.password = cw.InputBox(self, placeholder="Password", input_type="password",
-                                    placeholder_color="#c3c3c3", font=self.font)
-        self.password.pack()
-
-        ttk.Label(self, text="", font=("", 2)).pack()
-        self.confirm_password = cw.InputBox(self, placeholder="Confirm Password", input_type="password",
-                                            placeholder_color="#c3c3c3", font=self.font)
-        self.confirm_password.pack()
-
-        ttk.Label(self, text="", font=("", 2)).pack()
-        # display error message
-        self.error_msg = ttk.Label(self, text="error msg", font=("", 12), foreground="red")
-        self.error_msg.pack()
-
-        ttk.Label(self, text="", font=("", 2)).pack()
-
-        cw.Button(self, text="Sign Up", takefocus=0, width=15, font=("", 20),
-                  fg="white", fg_pressed="grey", bg="#299617", bg_hover="#0a6522", bg_pressed="#043927").pack()
+# class DriverRegistration(ttk.Frame):
+#     def __init__(self, container):
+#         self.font = ("", 20)
+#         style = ttk.Style()
+#         style.configure("TFrame", background="#d4d4d4")
+#         # create right frame to hold form box
+#         super().__init__(container, style="TFrame", padding=25)
+#
+#         # create form elements
+#         self.username = cw.InputBox(self, placeholder="Full Name", placeholder_color="#c3c3c3", font=self.font)
+#         self.username.pack()
+#         # add space
+#         ttk.Label(self, text="", font=("", 2)).pack()
+#
+#         self.gender = ttk.Combobox(self, values=["Male", "Female"], background="white", font=("", 15), state="readonly")
+#         self.gender.set("<<Select Gender>>")
+#         self.gender.bind("<<ComboboxSelected>>", lambda event: combo_select(self.gender))
+#         self.gender.pack(expand=1, fill="both")
+#         # add space
+#         ttk.Label(self, text="", font=("", 2)).pack()
+#
+#         self.username = cw.InputBox(self, placeholder="Email Address", placeholder_color="#c3c3c3", font=self.font)
+#         self.username.pack()
+#         # add space
+#         ttk.Label(self, text="", font=("", 2)).pack()
+#
+#         self.username = cw.InputBox(self, placeholder="Address", placeholder_color="#c3c3c3", font=self.font)
+#         self.username.pack()
+#         # add space
+#         ttk.Label(self, text="", font=("", 2)).pack()
+#
+#         self.username = cw.InputBox(self, placeholder="License ID", placeholder_color="#c3c3c3", font=self.font)
+#         self.username.pack()
+#         # add space
+#         ttk.Label(self, text="", font=("", 2)).pack()
+#
+#         self.username = cw.InputBox(self, placeholder="Username", placeholder_color="#c3c3c3", font=self.font)
+#         self.username.pack()
+#         # add space
+#         ttk.Label(self, text="", font=("", 2)).pack()
+#         self.password = cw.InputBox(self, placeholder="Password", input_type="password",
+#                                     placeholder_color="#c3c3c3", font=self.font)
+#         self.password.pack()
+#
+#         ttk.Label(self, text="", font=("", 2)).pack()
+#         self.confirm_password = cw.InputBox(self, placeholder="Confirm Password", input_type="password",
+#                                             placeholder_color="#c3c3c3", font=self.font)
+#         self.confirm_password.pack()
+#
+#         ttk.Label(self, text="", font=("", 2)).pack()
+#         # display error message
+#         self.error_msg = ttk.Label(self, text="error msg", font=("", 12), foreground="red")
+#         self.error_msg.pack()
+#
+#         ttk.Label(self, text="", font=("", 2)).pack()
+#
+#         cw.Button(self, text="Sign Up", takefocus=0, width=15, font=("", 20),
+#                   fg="white", fg_pressed="grey", bg="#299617", bg_hover="#0a6522", bg_pressed="#043927").pack()
